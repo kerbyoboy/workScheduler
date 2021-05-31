ded41d6eb3c01fec2d2e76f99e37070585b09dfd
kerbyoboy <kerbyhenry75@icloud.com>
2021-05-30 20:31:38 -0400

    html and css done

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// https://www.w3schools.com/jquery/
+$(document).ready(function(){
+    $('.saveBtn').on('click', function (){
+        console.log("saveBtn click")
+      //$(this).hide();
+    });
+  });
+ 
+
+
